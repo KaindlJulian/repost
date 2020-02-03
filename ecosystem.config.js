0faa1b3aa6266f3cbc77087084e967d7fd55a945
Julian Kaindl <kaindl745@gmail.com>
2020-02-03 14:35:50 +0100

    Initial Commit

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+// Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+module.exports = {
+  apps: [
+    {
+      name: 'unnamed',
+      script: './src/app.js',
+      instances: 1,
+      autorestart: true,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
