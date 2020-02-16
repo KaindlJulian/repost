1e8ac8da578f41d24cac103c5e18947a323855dc
Julian Kaindl <kaindl745@gmail.com>
2020-02-16 06:56:35 +0100

    Add tag args to launch config

--- src/bot/Bot.ts
+++ src/bot/Bot.ts
@@ -4,9 +4,9 @@ import { BotOptions, InstagramCredentials } from '../types';
 import { createInstagramPost, getImageAndText } from './tasks';
 import { Cache } from './Cache';
 
-const REDDIT_URL = 'https://www.reddit.com/r/';
-const TIME_ZONE = process.env.TIME_ZONE || 'Europe/Vienna';
 const CACHE_TTL = 60 * 60 * 24 * 7; // 7 days in seconds
+const TIME_ZONE = process.env.TIME_ZONE || 'Europe/Vienna';
+const REDDIT_URL = 'https://www.reddit.com/r/';
 
 export class Bot {
   /**
