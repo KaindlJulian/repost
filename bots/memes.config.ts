64e5db354442d0b226ed6c20d8aa3f1b31c1e02a
Julian Kaindl <kaindl745@gmail.com>
2020-03-19 02:50:24 +0100

    Load instagram credentials from env

--- bots/memes.config.ts
+++ bots/memes.config.ts
@@ -2,6 +2,5 @@ export const memes = {
   name: 'memes',
   instances: 1,
   merge_logs: true,
-  args:
-    '--subreddits memes dankmemes --schedule "0 0 * * SUN" --insta username:password --tags meme memes funny lol dankmemes',
+  args: `--subreddits memes dankmemes --schedule "0 10 * * *" --insta ${process.env.IG_TEST_USER}:${process.env.IG_TEST_PASS} --tags meme memes funny lmao dankmemes --explore`,
 };
