ef1e9180781bfe4aaafe490a836a48583cca019c
Julian Kaindl <kaindl745@gmail.com>
2020-03-30 00:28:19 +0200

    To please heroku

--- src/bot/tasks/task.config.ts
+++ src/bot/tasks/task.config.ts
@@ -5,6 +5,7 @@ const GALAXY_S5 = devices['Galaxy S5'];
 
 const LAUNCH_OPTIONS: LaunchOptions = {
   headless: process.env.NODE_ENV === 'production' ? true : false,
+  args: process.env.NODE_ENV === 'production' ? ['--no-sandbox'] : undefined,
   slowMo: process.env.NODE_ENV === 'production' ? undefined : 100,
 };
 
