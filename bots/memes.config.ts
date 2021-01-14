d745afa695028b6e17f01b7b64bcd90d2a40d6bc
Julian Kaindl <kaindl745@gmail.com>
2021-01-14 16:21:16 +0100

    Add new subreddits

--- bots/memes.config.ts
+++ bots/memes.config.ts
@@ -1,6 +1,6 @@
 export function memes() {
   return {
     name: 'memes',
-    args: `--subreddits memes,dankmemes,deepfriedmemes,wholesomememes --schedule "0 7,14,21 * * *" --insta ${process.env.IG_TEST_USER}:${process.env.IG_TEST_PASS} --tags meme,memes,funny,lmao,dankmemes --explore`,
+    args: `--subreddits memes,dankmemes,funny,wholesomememes,me_irl,toiletpaperusa --schedule "0 7,14,21 * * *" --insta ${process.env.IG_TEST_USER}:${process.env.IG_TEST_PASS} --tags meme,memes,funny,lmao,dankmemes --explore`,
   };
 }
